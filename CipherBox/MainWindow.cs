80d4407b59d3fb08f5669662fbeb824e830c4ad1
MisteryDush <i.arifdjanov@yandex.ru>
2022-07-05 00:47:59 +0500

    Initial Commit

---
+++ CipherBox/MainWindow.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace CipherBox
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
