2bc3196f3a08e25020dc93c463e4cb907ec3c1ef
MisteryDush <i.arifdjanov@yandex.ru>
2022-07-05 15:23:09 +0500

    Added new encryption - A1Z26

---
+++ EncryptionMethods/Methods/A1Z26/A1Z26.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace EncryptionMethods.Methods.A1Z26
+{
+    public partial class A1Z26 : Form
+    {
+        public A1Z26()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ EncryptionMethods/Methods/A1Z26/A1Z26Logic.cs
@@ -0,0 +1,7 @@
+﻿namespace EncryptionMethods.Methods.A1Z26
+{
+    public class A1Z26Logic
+    {
+        
+    }
+}
