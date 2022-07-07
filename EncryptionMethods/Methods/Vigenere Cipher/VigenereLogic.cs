c510893119ab85167f9a8a0b9c6d79152ccfa42a
MisteryDush <i.arifdjanov@yandex.ru>
2022-07-07 22:31:16 +0500

    Completed VigenereLogic decryption method.

--- EncryptionMethods/Methods/Vigenere Cipher/VigenereLogic.cs
+++ EncryptionMethods/Methods/Vigenere Cipher/VigenereLogic.cs
@@ -40,6 +40,45 @@ namespace EncryptionMethods.Methods.Vigenere_Cipher
                     }
                 }
             }
+            else
+            {
+                foreach (var ch in input)
+                {
+                    if (char.IsLetter(ch))
+                    {
+                        if (char.IsUpper(ch))
+                        {
+                            var indexLetter = AlphabetUpper.IndexOf(ch);
+                            var keyLetterIndex = AlphabetUpper.IndexOf(char.ToUpper(key[i % keyCount]));
+                            var newLetterIndex = indexLetter - keyLetterIndex;
+                            if (newLetterIndex < 0)
+                            {
+                                newLetterIndex = 26 - newLetterIndex;
+                            }
+                            var newLetter = AlphabetUpper[newLetterIndex];
+                            sb.Append(newLetter);
+                            i++;
+                        }
+                        else
+                        {
+                            var indexLetter = AlphabetLower.IndexOf(ch);
+                            var keyLetterIndex = AlphabetLower.IndexOf(char.ToLower(key[i % keyCount]));
+                            var newLetterIndex = indexLetter - keyLetterIndex;
+                            if (newLetterIndex < 0)
+                            {
+                                newLetterIndex = 26 + newLetterIndex;
+                            }
+                            var newLetter = AlphabetLower[newLetterIndex];
+                            sb.Append(newLetter);
+                            i++;
+                        }
+                    }
+                    else
+                    {
+                        sb.Append(ch);
+                    }
+                }
+            }
 
             return sb.ToString();
         }
