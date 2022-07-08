0274f1ce0bc89ac5e0a5fbe7a25275630abe5866
MisteryDush <i.arifdjanov@yandex.ru>
2022-07-08 22:53:27 +0500

    Completed encryption method.

--- EncryptionMethods/Methods/DateTimeCipher/DateTimeCipherLogic.cs
+++ EncryptionMethods/Methods/DateTimeCipher/DateTimeCipherLogic.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Runtime.Serialization;
 using System.Text;
+using System.Windows.Forms;
 using EncryptionMethods.Methods.ROT13;
 using EncryptionMethods.Methods.Vigenere_Cipher;
 
@@ -8,6 +9,8 @@ namespace EncryptionMethods.Methods.DateTimeCipher
 {
     public static class DateTimeCipherLogic
     {
+
+        private static string[] Numbers = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};        
         public static string[] Controller(bool mode, string input, string key)
         {
             var arr = mode ? Encrypt(input) : Decrypt(input, key);
@@ -25,8 +28,10 @@ namespace EncryptionMethods.Methods.DateTimeCipher
             // Step 1. Use hours to encrypt using ROT, e.g. 5pm will be ROT with the key of 17
             var iter1 = ROTLogic.Rot(input, true, int.Parse(time[0]));
             // Step 2. Use minutes and seconds as a key for Vigenere Cipher, first iteration is using minutes, second is using seconds.
-            var iter2 = VigenereLogic.Encrypt(iter1, time[1]);
-            var iter3 = VigenereLogic.Encrypt(iter2, time[2]);
+            var minsToString = $"{Numbers[int.Parse(char.GetNumericValue(time[1][0]).ToString())]}{Numbers[int.Parse(char.GetNumericValue(time[1][1]).ToString())]}";
+            var iter2 = VigenereLogic.Encrypt(iter1, minsToString);
+            var secToString = $"{Numbers[int.Parse(char.GetNumericValue(time[2][0]).ToString())]}{Numbers[int.Parse(char.GetNumericValue(time[2][1]).ToString())]}";
+            var iter3 = VigenereLogic.Encrypt(iter2, secToString);
             return new[] {iter3, string.Join("", time)};
 
         }
